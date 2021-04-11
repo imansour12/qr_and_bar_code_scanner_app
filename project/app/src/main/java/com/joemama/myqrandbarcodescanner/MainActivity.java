ce0ef3cd07348a22cbbcdc3cfe535fd5b68753f0
Mohamed Iyed Mansour <iyedmansour0@gmail.com>
2021-04-11 15:09:17 +0100

    Requests user to allow access to the camera

--- project/app/src/main/java/com/joemama/myqrandbarcodescanner/MainActivity.java
+++ project/app/src/main/java/com/joemama/myqrandbarcodescanner/MainActivity.java
@@ -1,16 +1,28 @@
 package com.joemama.myqrandbarcodescanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
     }
 
     public void ScanButton(View view){
